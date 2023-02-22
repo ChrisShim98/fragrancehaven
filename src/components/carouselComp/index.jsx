d51ffa36e516d4a9f9e0871fbfba59752c7fa1b0
Chris Shim <94628616+ChrisShim98@users.noreply.github.com>
2023-02-22 00:04:07 -0500

    Initial Commit

---
+++ src/components/carouselComp/index.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Pic1 from './pic1.png'
+import Pic2 from './pic2.png'
+import Pic3 from './pic3.png'
+import './index.css'
+
+const CarouselComp = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+  return (
+    <div className="pt-16 w-screen">
+        <Carousel 
+                infiniteLoop 
+                useKeyboardArrows 
+                showArrows={false}
+                showThumbs={false}
+                showStatus={false}
+                transitionTime={1000}
+                interval={5000}
+                animationHandler={'fade'}
+                onChange={setCurrentSlide}
+                swipeable={false}
+                autoPlay
+                className="p-0">
+                
+                <div>
+                    <img src={Pic1} className="photo" alt="" />
+                </div>
+                <div>
+                    <img src={Pic2} className="photo" alt="" />
+                </div>
+                <div>
+                    <img src={Pic3} className="photo" alt="" />
+                </div>
+            </Carousel>
+    </div>
+  )
+}
+
+export default CarouselComp
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BsSearch, BsCart } from "react-icons/bs";
+import Logo from './Logo.png'
+
+const Navbar = () => {
+  return (
+    <div className="w-screen h-16 fixed grid grid-flow-col bg-dark text-light place-items-center z-20">
+      <img src={Logo} alt="Logo" className="h-16" />
+      <div className="flex">
+        <input type="text" className="rounded-l-md h-8 w-[50vw] text-dark px-2" />
+        <div
+          className="bg-accent h-8 w-8 flex place-items-center justify-center rounded-r-md
+                hover:cursor-pointer hover:bg-[#fff6f6] hover:text-[#202020]"
+        >
+          <BsSearch />
+        </div>
+      </div>
+      <ul className="flex gap-2">
+        <li>Sign In</li>
+        <li>Help</li>
+        <h1 className="flex place-items-center"><BsCart />Cart</h1>    
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
