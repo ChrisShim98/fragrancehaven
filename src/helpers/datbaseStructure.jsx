5dcdd8fae20e5d4aebb19f61c3368b47a8d953dd
Chris Shim <94628616+ChrisShim98@users.noreply.github.com>
2024-02-04 15:40:20 -0500

    database details update

--- src/helpers/datbaseStructure.jsx
+++ src/helpers/datbaseStructure.jsx
@@ -13,11 +13,10 @@ const products = [
     description:
       "Versace Pour Homme Dylan Blue is a fresh and vibrant fragrance for men. It is a blend of citrus notes, including bergamot, grapefruit, and lemon, which create a sparkling and energetic opening. The heart of the fragrance is a blend of lavender, geranium, and cardamom, which add depth and complexity to the scent. The base of the fragrance is a blend of vetiver, cedarwood, and oakmoss, which provide a warm and woody finish. This fragrance is perfect for any occasion, whether it's a day at the office or a night out on the town. It is a versatile fragrance that can be worn year-round and is sure to turn heads.",
     img: "https://res.cloudinary.com/dhpo94oka/image/upload/v1706579964/FragranceHaven/Dev/bpwy5d9k7ixerh9v68wy.webp",
-    rating: reviews[0].rating,
-    review: [reviews[0]],
+    reviews: [reviews[0]],
     amount: 1,
-    inUserCart: [users[0]],
-    dateAdded: "2015-05-16T05:50:06"
+    dateAdded: "2015-05-16T05:50:06",
+    inUserCart: [users[0]]
   },
 ];
 
@@ -35,7 +34,7 @@ const brands = [
 const reviews = [
     {
         id: "1",
-        review: "This was nice",
+        message: "This was nice",
         rating: 4,
         product: products[0],
         reviewer: users[0],
@@ -52,7 +51,17 @@ const users = [
         password: "this is hashed",
         role: "customer",
         reviews: [reviews[0]],
-        cart: [products[0]]
+        cart: [products[0]],
+        transactions: [transaction[0]]
     }
+]
+
+const transaction = [
+  {
+    id: "1",
+    product: products[0],
+    user: users[0],
+    datePurchased: "2015-05-16T05:50:06"
+  }
 ]
 
